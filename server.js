5316049eb19f0723222b751f1601a322d95efb5a
TNQuocKhanh <khanh.tran@nexpando.com>
2022-10-16 19:33:53 +0700

    update all api 16/10

---
+++ server.js
@@ -0,0 +1,25 @@
+const app = require('./app')
+const dotenv = require('dotenv')
+const connectDatabase = require('./config/database')
+
+process.on('uncaughtException', err => {
+    console.log(`ERROR: ${err.stack}`);
+    console.log('Shutting down due to uncaught exception');
+    process.exit(1)
+})
+
+dotenv.config({ path: 'config/config.env' })
+
+connectDatabase()
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`)
+})
+
+process.on('unhandledRejection', err => {
+    console.log(`ERROR: ${err.stack}`);
+    console.log('Shutting down the server due to Unhandled Promise rejection');
+    server.close(() => {
+        process.exit(1)
+    })
+})
